41b80635d9ef4abfc9ced7948f5c75cf801637a2
Ricky <rickycorrigan@icloud.com>
2018-08-21 23:07:10 +0100

    tests completed for hero health and favorite food

--- hero.js
+++ hero.js
@@ -1,5 +1,7 @@
-const hero = function(name) {
+const hero = function(name,health,favorite_food) {
   this.name = name;
+  this.health = health;
+  this.favorite_food = favorite_food;
 }
 
 module.exports = hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -4,11 +4,19 @@ const hero = require('../hero.js')
 describe("hero test", function(){
 
   beforeEach(function(){
-    hero1 = new hero("John Smeaton");
+    hero1 = new hero("John Smeaton", "100%", "kebab");
   })
 
   it("hero has a name", function(){
     assert.strictEqual(hero1.name, "John Smeaton");
+  })
+
+  it("hero has health", function(){
+    assert.strictEqual(hero1.health, "100%");
+  })
+
+  it("hero's favorite food", function(){
+    assert.strictEqual(hero1.favorite_food, "kebab");
   })
 
 })
