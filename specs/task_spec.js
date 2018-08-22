96f617244ace0066bef365ab809585d34a217eb8
Ricky <rickycorrigan@icloud.com>
2018-08-22 07:45:46 +0100

    test pass for reward value

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -4,7 +4,7 @@ const task = require ('../task.js');
 describe("task test", function(){
 
   beforeEach(function(){
-  task1 = new task(10, 5);
+  task1 = new task(10, 5, 90);
 })
 
 it("task has difficulty level", function(){
@@ -14,5 +14,11 @@ it("task has difficulty level", function(){
 it("task has urgency level", function(){
   assert.strictEqual(task1.urgency_level, 5);
 })
+
+it("task has reward value", function(){
+  assert.strictEqual(task1.reward_value, 90);
+})
+
+
 
 })
--- task.js
+++ task.js
@@ -1,6 +1,7 @@
-const task = function(difficulty_level,urgency_level){
+const task = function(difficulty_level,urgency_level,reward_value){
   this.difficulty_level = difficulty_level;
   this.urgency_level = urgency_level;
+  this.reward_value = reward_value;
 }
 
 module.exports = task;
