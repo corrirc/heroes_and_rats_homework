32a31ee511d65a5e36214d41c5410ba46946d45b
Ricky <rickycorrigan@icloud.com>
2018-08-22 07:31:37 +0100

    test passes for urgency level and difficulty level

---
+++ specs/task_spec.js
@@ -0,0 +1,18 @@
+const assert = require ('assert');
+const task = require ('../task.js');
+
+describe("task test", function(){
+
+  beforeEach(function(){
+  task1 = new task(10, 5);
+})
+
+it("task has difficulty level", function(){
+  assert.strictEqual(task1.difficulty_level, 10);
+})
+
+it("task has urgency level", function(){
+  assert.strictEqual(task1.urgency_level, 5);
+})
+
+})
---
+++ task.js
@@ -0,0 +1,6 @@
+const task = function(difficulty_level,urgency_level){
+  this.difficulty_level = difficulty_level;
+  this.urgency_level = urgency_level;
+}
+
+module.exports = task;
